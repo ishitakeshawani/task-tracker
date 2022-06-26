3fca60dddd826fe4edb4019e225ffe859fe80260
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-06-26 15:21:46 +0530

    initialize angular app

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'i will',
+        day: 'May 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'learn',
+        day: 'May 6th at 2:30pm',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'typescript',
+        day: 'May 7th at 2:30pm',
+        reminder: false
+    }
+]
