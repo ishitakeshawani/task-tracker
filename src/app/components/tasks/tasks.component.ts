47d7023b60415f23dbe7f88cd045147e6b9fa139
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-06-26 16:15:33 +0530

    added task service

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { TASKS } from "../../mock-tasks"
 import { Task } from "../../Task"
+import { TaskService } from 'src/app/services/task.service';
+
+
 
 @Component({
   selector: 'app-tasks',
@@ -8,11 +10,12 @@ import { Task } from "../../Task"
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks)
   }
 
 }
---
+++ src/app/services/task.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { TASKS } from "../mock-tasks"
+import { Task } from "../Task"
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor() { }
+
+  getTasks(): Observable<Task[]> {
+    const tasks = of(TASKS);
+    return tasks;
+  }
+}
